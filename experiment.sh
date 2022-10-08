16980fcd0eb09e5b25f6da1152b8fae84000b999
RaccoonOnion <ryan.yunxiang.yan@gmail.com>
2022-10-08 18:41:09 +0800

    shell code with functions

--- experiment.sh
+++ experiment.sh
@@ -1,10 +1,13 @@
 # This is a simple shell code for running experients
 #!/bin/bash
-readonly FOLDER1="data/test"
-readonly FOLDER2="results"
+# readonly FOLDER2="results"  way to create a constant variable in shell script
+
+
+RunExperiments() {
+
 flag=0
 
-cd $FOLDER1
+cd data/test
 
 for FILE in *;
 do
@@ -18,11 +21,43 @@ do
 	fi
 done
 
-pwd
+pwd #sample-proj/results
 cd ../
 for FILE in data/test/*;
 do
 	echo $FILE
 	python src/test.py $FILE 0.75
 done
+
+}
+
+###
+
+for arg in "$@"; do
+  if [[ "$arg" = -r ]] || [[ "$arg" = --run-experiments ]]; then
+    ARG_RUN_EXPERIMENTS=true
+  fi
+  if [[ "$arg" = -z ]] || [[ "$arg" = --zip-file ]]; then
+    ARG_ZIP_FILE=true
+  fi
+  if [[ "$arg" = -a ]] || [[ "$arg" = --aws-copy ]]; then
+    ARG_AWS_COPY=true
+  fi
+done
+
+###
+
+if [[ "$ARG_RUN_EXPERIMENTS" = true ]]; then
+  RunExperiments
+fi
+
+if [[ "$ARG_ZIP_FILE" = true ]]; then
+  ZipFile
+fi
+
+if [[ "$ARG_AWS_COPY" = true ]]; then
+  AwsCopy
+fi
+
+
 
